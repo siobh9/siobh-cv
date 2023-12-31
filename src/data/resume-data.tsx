16c5f41d8179c0cc4b373479d8e04f96cf399461
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-12-30 20:27:31 -0500

    chore: update summary

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -8,7 +8,7 @@ export const RESUME_DATA = {
   about:
     "Software Engineer",
   summary:
-    "I like to build cool, open-source things.",
+    "I like to build cool, open-source things with good people.",
   avatarUrl: "https://avatars.githubusercontent.com/seanmc9 ",
   personalWebsiteUrl: "https://github.com/seanmc9",
   contact: {
